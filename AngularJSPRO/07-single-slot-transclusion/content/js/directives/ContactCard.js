05ca4a81b77fec2545171eb6e2bcc8c9bdab0642
danielflorez111 <danielflorez111@hotmail.com>
2017-05-07 23:10:52 -0500

    06 07

---
+++ AngularJSPRO/07-single-slot-transclusion/content/js/directives/ContactCard.js
@@ -0,0 +1,16 @@
+function ContactCard() {
+    return {
+        transclude: true,
+        template: `
+            <div>
+                <h4>Contact</h4>
+                <div ng-transclude></div>
+                <p>Test</p>
+            </div
+        `
+    }
+}
+
+angular
+    .module("app")
+    .directive("contactCard", ContactCard);
---
+++ AngularJSPRO/08-manual-transclusion/content/js/directives/ContactCard.js
@@ -0,0 +1,25 @@
+function ContactCard() {
+    return {
+        transclude: true,
+        template: `
+            <div>
+                <span></span>
+                <div></div>
+                <p>Test</p>
+            </div
+        `,
+        link: function($scope, $element, $attrs, $ctrl, $transclude){
+            var div = $element.find("div");
+            var span = $element.find("span");
+            var cloned = $transclude(function(elements) {
+                elements[1].textContent = elements[1].textContent.toUpperCase();s 
+            });
+            span.append(cloned[1]);
+            div.append(cloned[3]);
+        }
+    }
+}
+
+angular
+    .module("app")
+    .directive("contactCard", ContactCard);
