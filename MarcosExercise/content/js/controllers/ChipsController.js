76f587a655cca383a3b825752bd3d96106b880b0
danielflorez111 <danielflorez111@hotmail.com>
2017-05-15 22:10:53 -0500

    Exercise finished

--- MarcosExercise/content/js/controllers/ChipsController.js
+++ MarcosExercise/content/js/controllers/ChipsController.js
@@ -2,6 +2,7 @@ function ChipsController ($timeout, $q) {
 
     this.names = [];
     this.selectNames = [];
+    this.listNames = [];
     this.requireMatch = true; 
 
     this.addName = function(chip) {
@@ -12,8 +13,20 @@ function ChipsController ($timeout, $q) {
         angular.copy(this.names, this.selectNames)
     };
 
-    this.printSelectedName = function() {
-        console.log(this.name)
+    this.addToList = function() {
+        this.listNames.indexOf(this.name) === -1 ? this.listNames.push(this.name) : 0;
+        var index = this.selectNames.indexOf(this.name);
+        if(index != -1) {
+            this.selectNames.splice(index, 1 );
+        }  
+    };
+
+    this.removeFromList = function(name){
+        this.selectNames.indexOf(name) === -1 ? this.selectNames.push(name) : 0;
+        var index = this.listNames.indexOf(name);  
+        if(index != -1) {
+            this.listNames.splice(index, 1 );
+        }              
     };
 
     this.querySearch = function(searchText) {
