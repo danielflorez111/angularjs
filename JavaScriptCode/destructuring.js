457882a4f9c28df1d439a2d29691df0d1d458a3a
Daniel Felipe Florez Salgado <dflorez@yuxiglobal.com>
2017-06-13 17:34:33 -0500

    ES6

---
+++ JavaScriptCode/destructuring.js
@@ -0,0 +1,12 @@
+const point = [10, 25, -34];
+const [x, y, z] = point;
+console.log(x, y, z);
+
+
+const gemstone = {
+  type: 'quartz',
+  color: 'rose',
+  karat: 21.29
+};
+const {type, color, karat} = gemstone;
+console.log(type, color, karat);
---
+++ JavaScriptCode/loops.js
@@ -0,0 +1,20 @@
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for
+for (let i = 0; i < digits.length; i++) {
+  console.log(digits[i]);
+}
+
+console.log("");
+
+// for in
+for (const index in digits) {
+  console.log(digits[index]);
+}
+
+console.log("");
+
+// for of
+for (const digit of digits) {
+  console.log(digit);
+}
