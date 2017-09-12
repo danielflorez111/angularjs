ce308b4531573366bfb128c7c6f866646f01b6f3
Daniel Felipe Florez Salgado <danielflorez111@hotmail.com>
2017-09-12 16:53:52 -0500

    Hola mundo

---
+++ Angular2/02TypeScript/classes.js
@@ -0,0 +1,11 @@
+"use strict";
+var Avenger = /** @class */ (function () {
+    function Avenger(nombre, equipo, nombreReal) {
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nombreReal = nombreReal;
+    }
+    return Avenger;
+}());
+var antman = new Avenger("Antman", "Cap", "Scott Lang");
+console.log(antman);
---
+++ Angular2/02TypeScript/classes.ts
@@ -0,0 +1,18 @@
+class Avenger {
+    nombre:string;
+    equipo:string;
+    nombreReal:string;
+
+    puedePelear:boolean;
+    peleasGanadas:number;
+
+    constructor(nombre:string, equipo:string, nombreReal:string) {
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nombreReal = nombreReal;
+    }
+}
+
+let antman:Avenger = new Avenger("Antman", "Cap", "Scott Lang");
+
+console.log(antman);
