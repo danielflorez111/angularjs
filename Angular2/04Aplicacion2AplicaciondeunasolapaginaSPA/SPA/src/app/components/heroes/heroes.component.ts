99c4403d612b5de62bf50c3a5c210db4883a6675
Daniel Felipe Florez Salgado <danielflorez111@hotmail.com>
2017-10-06 18:45:33 -0500

    Route

--- Angular2/04Aplicacion2AplicaciondeunasolapaginaSPA/SPA/src/app/components/heroes/heroes.component.ts
+++ Angular2/04Aplicacion2AplicaciondeunasolapaginaSPA/SPA/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -10,11 +12,14 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor(private _heroesService:HeroesService) {}
+  constructor(private _heroesService:HeroesService, private router:Router) {}
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
+  }
+
+  verHeroe(idx:number) {
+    this.router.navigate(['/heroe', idx]);
   }
 
 }
