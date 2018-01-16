773f567419cd053b73ec2452d0f0bb98730c1863
danielflorez111 <danielflorez111@hotmail.com>
2018-01-16 15:12:54 -0500

    Angular Flex

--- Angular2/00AngularMaterial/angularmaterial/src/app/app.module.ts
+++ Angular2/00AngularMaterial/angularmaterial/src/app/app.module.ts
@@ -3,12 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+
 
 import { AppComponent } from './app.component';
 
@@ -21,12 +18,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatCheckboxModule,
-    MatRadioModule, 
-    MatSelectModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule
+    FlexLayoutModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
