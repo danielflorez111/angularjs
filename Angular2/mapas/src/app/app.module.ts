a3b2206ed55a52eaeac9e1007a0076ea0ad60df2
danielflorez <daniel.florez@yuxiglobal.com>
2018-04-24 16:54:21 -0500

    Maps

---
+++ Angular2/mapas/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { MaterialModule } from './material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapaComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBxa_9f44lAmCRS2HNo_ipW2EsJx6J2LJ4'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular2/mapas/src/app/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatCardModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatCardModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
