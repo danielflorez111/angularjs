20a5232afdcc28e244d9a78d616b40729616c6f9
danielflorez <daniel.florez@yuxiglobal.com>
2018-04-25 10:50:29 -0500

    Maps, local storage

--- Angular2/mapas/src/app/components/mapa/mapa.component.ts
+++ Angular2/mapas/src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 import { Marcador } from '../../classes/marcador.class';
 
 @Component({
@@ -12,9 +13,13 @@ export class MapaComponent implements OnInit {
   lat: number = 51.678418;
   lng: number = 7.809007;
 
-  constructor() {
-    const nuevoMarcador = new Marcador(51.678418, 7.809007);
-    this.marcadores.push(nuevoMarcador);
+  constructor(public snackBar: MatSnackBar) {
+    // const nuevoMarcador = new Marcador(51.678418, 7.809007);
+    // this.marcadores.push(nuevoMarcador);
+
+    if (localStorage.getItem('marcadores')) {
+      this.marcadores = JSON.parse(localStorage.getItem('marcadores'));
+    }
   }
 
   ngOnInit() {
@@ -24,6 +29,19 @@ export class MapaComponent implements OnInit {
     const coords: { lat: number, lng: number } = event.coords;
     const nuevoMarcador = new Marcador(coords.lat, coords.lng);
     this.marcadores.push(nuevoMarcador);
+
+    this.guardarStorage();
+    this.snackBar.open('Marcador agregado', 'Cerrar', { duration: 1000 });
+  }
+
+  borrarMarcador(i: number) {
+    this.marcadores.splice(i, 1);
+    this.guardarStorage();
+    this.snackBar.open('Marcador borrado', 'Cerrar', { duration: 1000 });
+  }
+
+  guardarStorage() {
+    localStorage.setItem('marcadores', JSON.stringify(this.marcadores));
   }
 
 }
