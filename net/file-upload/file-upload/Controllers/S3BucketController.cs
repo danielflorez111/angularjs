1d3f1ea06fb7c0d4b0a04e0e8b52d68b29bfe30e
danielflorez <daniel.florez@yuxiglobal.com>
2018-11-16 16:27:45 -0500

    S3

---
+++ net/file-upload/file-upload/Controllers/S3BucketController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace file_upload.Controllers
+{
+    [Route("api/S3Bucket")]
+    public class S3BucketController : ApiController
+    {
+        public IHttpActionResult CreateBucket()
+        {
+            return Ok();
+        }
+
+    }
+}
